e8d6aa0e9813ddd4dc567a17d1fc16c61ddc0a42
ndibbern <natidibbern@gmail.com>
2017-02-14 11:04:22 -0800

    Add file

---
+++ MathMethods.java
@@ -0,0 +1,43 @@
+import java.math.BigInteger;
+
+public class MathMethods {
+
+    public MathMethods() {
+    }
+
+    public static BigInteger factorial(int n) {
+        BigInteger nfac = n;
+        for(int i = n - 1; i > 1; )
+        return 1;
+    }
+
+    public static long gcd(long m, long n) {
+
+        return -5;
+    }
+
+    public static long lcm(long m, long n) {
+
+        return -5;
+    }
+
+    public static double poly(double x, double[] coeff) {
+
+        return -0.5;
+    }
+
+    public static double power(double x, int n) {
+
+        return 0.5;
+    }
+
+    public static double root(int n, double x, double epsilon) {
+
+        return 0.5;
+    }
+
+    public static double sqrt(double x, double epsilon) {
+
+        return 0.5;
+    }
+}
