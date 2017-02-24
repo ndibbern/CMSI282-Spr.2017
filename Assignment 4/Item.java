00520f6e6cfc218531ffeebad93fe9088ea825ff
Natalia Dibbern <ndibbern@natalias-mbp.attlocal.net>
2017-02-23 19:19:34 -0800

    fix item class

--- Assignment 4/Item.java
+++ Assignment 4/Item.java
@@ -1,19 +1,11 @@
 public class Item {
 
-    private static final String DEFAULT_LABEL = "Unbekannt";
-
-    private String label; // may be unnecesary because instances have names.
     private int cost;
     private int weight;
 
-    public Item (String label, int cost, int weight) {
-        this.label = label;
+    public Item (int cost, int weight) {
         this.cost = cost;
         this.weight = weight;
-    }
-
-    public Item (int cost, int weight) {
-        this(DEFAULT_LABEL, cost, weight);
     }
 
     public int getCost () {
