fec4a28b298d9ae9b42395f8ba1ba88bd89dfd17
Natalia Dibbern <ndibbern@natalias-mbp.attlocal.net>
2017-02-23 19:15:51 -0800

    add SumoSolver

---
+++ Assignment 4/SumoSolver.java
@@ -0,0 +1,16 @@
+public class SumoSolver {
+
+    public static void main (String[] args) {
+        /*  1) get items in the store
+            2) get ammount to be spent
+            3) put them in store ArrayLis
+            4) make a table for DP: store.size x ammount
+            5) fill in the table with new carts with DP:
+                5.1) can I buy this item with this money?
+                5.2) add the solution for the money that remains (use clone and add 0 or 1 from 5.1)
+                5.3) compare with solution without this item (on top) and replace if necessary
+        */
+
+    }
+
+}
